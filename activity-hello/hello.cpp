fbce023f685706d00846b82e7ad37746e267087d
Ethan Mendel <emendel@uncc.edu>
2022-01-12 11:07:23 -0500

    code and executable

--- activity-hello/hello.cpp
+++ activity-hello/hello.cpp
@@ -1,4 +1,12 @@
-int main () {
+#include <iostream>
+#include <unistd.h>
 
+int main () {
+  int returnCode;
+  char name[50];
+  returnCode = gethostname(name,sizeof(name));
+  if(returnCode == 0){
+    std::cout<<"hostName: "<<name<<std::endl;
+  }
   return 0;
 }
