301ff280e7f5a4cfc9998ea1c404220551eedc21
Ethan Mendel <ethanmendel516@gmail.com>
2022-02-26 12:39:40 -0500

    add bubble sort code

--- activity-bubblesort/bubblesort/bubblesort.cpp
+++ activity-bubblesort/bubblesort/bubblesort.cpp
@@ -5,6 +5,7 @@
 #include <iostream>
 #include <unistd.h>
 #include "omploop.hpp"
+#include <mutex>
 
 #ifdef __cplusplus
 extern "C" {
@@ -17,6 +18,11 @@ extern "C" {
 }
 #endif
 
+void swap(int* arr, int i, int j) {
+  int temp = arr[i];
+  arr[i] = arr[j];
+  arr[j] = temp;
+}
 
 int main (int argc, char* argv[]) {
   if (argc < 3) { std::cerr<<"usage: "<<argv[0]<<" <n> <nbthreads>"<<std::endl;
@@ -24,17 +30,60 @@ int main (int argc, char* argv[]) {
   }
 
   int n = atoi(argv[1]);
+  int numThreads = atoi(argv[2]);
   
   // get arr data
   int * arr = new int [n];
   generateMergeSortData (arr, n);
 
   //insert sorting code here.
+  std::chrono::time_point<std::chrono::system_clock> start = std::chrono::system_clock::now();
+  OmpLoop omp;
+  omp.setNbThread(numThreads);
+  omp.setGranularity(2);
 
+  int swapped = 1;
+  std::mutex swappedMutex;
+  while (swapped) {
+    swapped = 0;
+    omp.parfor<int>(0, n, 2,
+      [&](int tls) -> void {
+        tls = 0;
+      },
+      [&](int i, int tls) -> void {
+        if(arr[i] > arr[i+1]){
+          swap(arr,i,i+1);
+          tls = 1;
+        }
+      },
+      [&](int tls) -> void {
+        swapped = swapped || tls;
+      }
+    );
+    omp.parfor<int>(1,n-3,2,
+      [&](int tls) -> void {
+        tls = 0;
+      },
+      [&](int i, int tls) -> void {
+        if(arr[i] > arr[i+1]){
+          swap(arr,i,i+1);
+          tls = 1;
+        }
+      },
+      [&](int tls) -> void {
+        swapped = swapped || tls;
+      }
+    );
+  }
+
+  std::chrono::time_point<std::chrono::system_clock> end = std::chrono::system_clock::now();
+  std::chrono::duration<double> elpased_seconds = end-start;
+  //end of code
 
   
   checkMergeSortResult (arr, n);
-  
+  std::cerr<<elpased_seconds.count()<<std::endl;
+
   delete[] arr;
 
   return 0;
