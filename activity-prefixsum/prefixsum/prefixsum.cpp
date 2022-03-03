0863c2c4e4bb4a8b0f3582435e7876f48d400aee
Ethan Mendel <ethanmendel516@gmail.com>
2022-03-03 15:46:23 -0500

    try sequential prefixsum

--- activity-prefixsum/prefixsum/prefixsum.cpp
+++ activity-prefixsum/prefixsum/prefixsum.cpp
@@ -5,6 +5,7 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include "omploop.hpp"
+#include <math.h>
 
 #ifdef __cplusplus
 extern "C" {
@@ -24,15 +25,34 @@ int main (int argc, char* argv[]) {
 
   
   int n = atoi(argv[1]);
+  int numThreads = atoi(argv[2]);
 
   int * arr = new int [n];
   generatePrefixSumData (arr, n);
 
   int * pr = new int [n+1];
 
-  //insert prefix sum code here
-
-  
+  pr[0] = 0;
+  float frac = n/numThreads;
+  int num_in_each_process = ceil(frac);
+  for(int i=0;i<numThreads;i++){
+      int start_index = i * num_in_each_process;
+      pr[start_index] = 0;
+      for(int j=0;j<num_in_each_process;j++){
+          if(start_index + j + 1 < n){
+              pr[start_index + j + 1] = pr[start_index + j] + arr[start_index + j];
+          }
+      }
+  }
+  for(int i=1;i<numThreads;i++){
+      int start_index = i * num_in_each_process;
+      int diff = pr[start_index - 1];
+      for(int j=0;j<num_in_each_process;j++){
+          if(start_index + j + 1 < n){
+              pr[start_index + j] += diff;
+          }
+      }
+  }
   
   checkPrefixSumResult(pr, n);
 
