7e60aad25de6673b869713a5c6296abda7e9273b
Ethan Mendel <emendel@uncc.edu>
2022-03-22 20:07:54 -0400

    edit code

--- assignment-mpi-hello-world/hello_world/hello.cpp
+++ assignment-mpi-hello-world/hello_world/hello.cpp
@@ -7,11 +7,11 @@ int main(int argc, char*argv[]) {
 
   int P;
   int i;
-  int mach;
 
   MPI_Comm_size(MPI_COMM_WORLD, &P);
   MPI_Comm_rank(MPI_COMM_WORLD, &i);
-  MPI_Get_processor_name(MPI_COMM_WORLD, &mach);
 
-  std::cout<<"I am process "<<i<<" out of "<<P<<". I am running on "<<mach;
+  std::cout<<"I am process "<<i<<" out of "<<P<<". I am running on "<<gethostname<<std::endl;
+
+  MPI_Finalize();
 }
