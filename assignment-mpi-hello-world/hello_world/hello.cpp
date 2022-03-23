7b071db11973f9195c5e38fff7882bf1bb52c5c5
Ethan Mendel <ethanmendel516@gmail.com>
2022-03-22 20:01:49 -0400

    code

--- assignment-mpi-hello-world/hello_world/hello.cpp
+++ assignment-mpi-hello-world/hello_world/hello.cpp
@@ -3,5 +3,15 @@
 #include <iostream>
 
 int main(int argc, char*argv[]) {
+  MPI_Init(&argc, &argv);
 
+  int P;
+  int i;
+  int mach;
+
+  MPI_Comm_size(MPI_COMM_WORLD, &P);
+  MPI_Comm_rank(MPI_COMM_WORLD, &i);
+  MPI_Get_processor_name(MPI_COMM_WORLD, &mach);
+
+  std::cout<<"I am process "<<i<<" out of "<<P<<". I am running on "<<mach;
 }
