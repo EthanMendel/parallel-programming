498eb6dfc9305573aff116e3f763ac85b32f1883
Ethan Mendel <emendel@uncc.edu>
2022-04-13 15:50:45 -0400

    tests passed

--- assignment-mpi-advanced-manager-worker/advanced_manager_worker/mpi_advanced_manager_worker.cpp
+++ assignment-mpi-advanced-manager-worker/advanced_manager_worker/mpi_advanced_manager_worker.cpp
@@ -58,11 +58,11 @@ int main (int argc, char* argv[]) {
   int chunkNum = 3;
   int gran = 10;
   double sum = 0;
-  double* params = new double[chunkNum*7];
   int lastProcessed = 0;
   int numSent = 0;
   int numRecv = 0;
   if(i == 0){
+    double* params = new double[7];
     for(int c=0;c<chunkNum;c++){
       for(int i=1;i<P;i++){
         int s = lastProcessed;
@@ -76,14 +76,14 @@ int main (int argc, char* argv[]) {
         }
         lastProcessed = e;
 	//std::cout<<"sending "<<numSent+1<<" to process "<<i<<std::endl;
-	params[c*chunkNum+0] = funcID;
-        params[c*chunkNum+1] = a;
-        params[c*chunkNum+2] = b;
-        params[c*chunkNum+3] = n;
-        params[c*chunkNum+4] = intensity;
-        params[c*chunkNum+5] = s;
-        params[c*chunkNum+6] = e;
-	MPI_Send(&(params[c*chunkNum+0]), 7, MPI_DOUBLE, i, 111, MPI_COMM_WORLD);
+	params[0] = funcID;
+        params[1] = a;
+        params[2] = b;
+        params[3] = n;
+        params[4] = intensity;
+        params[5] = s;
+        params[6] = e;
+	MPI_Send(&(params[0]), 7, MPI_DOUBLE, i, 111, MPI_COMM_WORLD);
         numSent++;
       }
     }
@@ -121,24 +121,24 @@ int main (int argc, char* argv[]) {
     std::cout<<sum;
     std::cerr<<tTime.count();
   }else{
+    double* params = new double[chunkNum*7];
     MPI_Request* req = new MPI_Request[chunkNum];
     for(int c=0;c<chunkNum;c++){
-      MPI_Irecv(&(params[c*chunkNum+0]), 7, MPI_DOUBLE, 0, MPI_ANY_TAG, MPI_COMM_WORLD,&req[c]);
+      MPI_Irecv(&(params[c*7+0]), 7, MPI_DOUBLE, 0, MPI_ANY_TAG, MPI_COMM_WORLD,&req[c]);
     }
     MPI_Status s;
     MPI_Wait(&req[0],&s);//wait for the first request
     int chunkI = 0;
     while(s.MPI_TAG == 111){
-      for(int i=0;i<7;i++){
-        std::cout<<params[chunkI*chunkNum+i]<<",";
-      }
-      std::cout<<std::endl;
+      //for(int i=0;i<7;i++){
+      //  std::cout<<params[chunkI*chunkNum+i]<<",";
+      //}
+      //std::cout<<std::endl;
       double* res = new double[1];
-      res[0] = doIntegration(params[chunkI*chunkNum+0],params[chunkI*chunkNum+1],params[chunkI*chunkNum+2],params[chunkI*chunkNum+3],params[chunkI*chunkNum+4],params[chunkI*chunkNum+5],params[chunkI*chunkNum+6]);
-      std::cout<<"\tgot "<<res[0]<<std::endl;
+      res[0] = doIntegration(params[chunkI*7+0],params[chunkI*7+1],params[chunkI*7+2],params[chunkI*7+3],params[chunkI*7+4],params[chunkI*7+5],params[chunkI*7+6]);
+      //std::cout<<"\tgot "<<res[0]<<std::endl;
       MPI_Send(&(res[0]), 1, MPI_DOUBLE, 0, 111, MPI_COMM_WORLD);
-      int h = chunkI;
-      MPI_Irecv(&(params[h*chunkNum+0]), 7, MPI_DOUBLE, 0, MPI_ANY_TAG, MPI_COMM_WORLD, &req[h]);
+      MPI_Irecv(&(params[chunkI*7+0]), 7, MPI_DOUBLE, 0, MPI_ANY_TAG, MPI_COMM_WORLD, &req[chunkI]);
       chunkI = (chunkI + 1) % chunkNum;
       MPI_Wait(&req[chunkI],&s);
     }
